f639879aee103e9bd2fd8b54dcfa0f2b95be5728
Eugene Romaniuk <zhenyarom08@gmail.com>
2026-02-16 22:09:24 +0100

    Added ROC charts, thresholds for default classifications, Expected loss

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 from xgboost import XGBClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_predict
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
@@ -10,8 +11,48 @@ from sklearn.metrics import (
     roc_auc_score,
     f1_score,
     precision_score,
-    recall_score
+    recall_score,
+    roc_curve,
+    brier_score_loss
 )
+
+LGD = 0.45
+
+def find_optimal_threshold(model, X_train, y_train):
+    
+    oof_probs = cross_val_predict(model, X_train, y_train, cv=5, method='predict_proba')[:, 1]
+
+    thresholds = np.linspace(0, 1, 100)
+    scores = [f1_score(y_train, oof_probs >= t) for t in thresholds]
+
+    optimal_t = thresholds[np.argmax(scores)]
+    
+    return optimal_t
+
+def evaluate_model(model, X_test, y_test, threshold, EAD_series):
+    y_prob = model.predict_proba(X_test)[:, 1]
+    y_pred = (y_prob >= threshold).astype(int)
+
+    expected_loss = y_prob * LGD * EAD_series
+
+    total_exprected_loss = np.sum(expected_loss)
+
+    fpr, tpr, thresholds_roc = roc_curve(y_test, y_prob)
+
+    return {
+        'Accuracy': accuracy_score(y_test, y_pred),
+        'ROC-AUC': roc_auc_score(y_test, y_prob),
+        'F1': f1_score(y_test, y_pred),
+        'Precision': precision_score(y_test, y_pred),
+        'Recall': recall_score(y_test, y_pred),
+        'Brier Score': brier_score_loss(y_test, y_prob),
+        'Expected Loss': f'${total_exprected_loss:,.0f}',
+        'FPR': fpr,
+        'TPR': tpr,
+        'y_prob': y_prob
+    }
+
+
 
 df = pd.read_csv('data/credit_risk_dataset.csv')
 
@@ -40,6 +81,7 @@ scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
+
 log_model = LogisticRegression()
 log_model.fit(X_train_scaled, y_train)
 
@@ -50,29 +92,37 @@ rf_model.fit(X_train, y_train)
 
 #GRADIENT BOOSTING
 
-gb_model = XGBClassifier(random_state=123, class_weight="balanced")
+gb_model = XGBClassifier(random_state=123)
 gb_model.fit(X_train, y_train)
+
+#OPTIMIZING THRESHOLDS 
+
+threshold_log = find_optimal_threshold(log_model, X_train_scaled, y_train)
+threshold_rf = find_optimal_threshold(rf_model, X_train, y_train)
+threshold_gb = find_optimal_threshold(gb_model, X_train, y_train)
 
 #COMPARISON AND EVALUATION
 
-def evaluate_model(model, X_test, y_test):
-    y_pred = model.predict(X_test)
-    y_prob = model.predict_proba(X_test)[:, 1]
-
-    return {
-        'Accuracy': accuracy_score(y_test, y_pred),
-        'ROC-AUC': roc_auc_score(y_test, y_prob),
-        'F1': f1_score(y_test, y_pred),
-        'Precision': precision_score(y_test, y_pred),
-        'Recall': recall_score(y_test, y_pred)
-    }
-
 results = {}
 
-results['Logistic Regression'] = evaluate_model(log_model, X_test_scaled, y_test)
-results['Random Forest'] = evaluate_model(rf_model, X_test, y_test)
-results['Gradient Boosting'] = evaluate_model(gb_model, X_test, y_test)
+results['Logistic Regression'] = evaluate_model(log_model, X_test_scaled, y_test, threshold_log, X_test['loan_amnt'])
+results['Random Forest'] = evaluate_model(rf_model, X_test, y_test, threshold_rf, X_test['loan_amnt'])
+results['Gradient Boosting'] = evaluate_model(gb_model, X_test, y_test, threshold_gb, X_test['loan_amnt'])
 
 results_df = pd.DataFrame(results).T
 
-print(results_df)
+print(results_df.drop(['FPR', 'TPR', 'y_prob'], axis=1))
+
+fig, ax = plt.subplots(1, 3, figsize=(12, 6))
+
+for model_name, i in zip(results_df.index.to_list(), range(3)):
+    ax[i].plot(
+        results_df.at[model_name, 'FPR'],
+        results_df.at[model_name, 'TPR'],
+        label=f'ROC Curve (AUC = {results_df.at[model_name, 'ROC-AUC']:.2f})'
+    )
+    ax[i].plot([0, 1], [0, 1], linestyle="--")
+    ax[i].set_title(f'ROC Curve for {model_name}')
+    ax[i].set_xlabel('False Positive Rate'); ax[i].set_ylabel('True Positive Rate')
+
+plt.show()
